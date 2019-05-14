4c9dae6260e0d5af81035f8a6581cfe985a4ca95
rijwan <rijwanc007@gmail.com>
2019-05-14 14:54:05 +0600

    second_change

--- assets/js/custom_script.js
+++ assets/js/custom_script.js
@@ -1,3 +1,14 @@
 function _(x){
     return document.getElementById(x);
 }
+jQuery(document).ready(function(){
+    jQuery('#slider').skdslider({
+        slideSelector: '.slide',
+        delay:5000,
+        animationSpeed:2000,
+        showNextPrev:true,
+        showPlayButton:true,
+        autoSlide:true,
+        animationType:'fading'
+    });
+});
