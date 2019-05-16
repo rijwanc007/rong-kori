986833c25a6c477935fbec8d6d60a070310b6e5e
rijwan <rijwanc007@gmail.com>
2019-05-16 08:59:43 +0600

    fourth_change

--- assets/js/custom_script.js
+++ assets/js/custom_script.js
@@ -12,3 +12,41 @@ jQuery(document).ready(function(){
         animationType:'fading'
     });
 });
+var right = 0;
+var maxMargin;
+var jumpMargin = 150;
+function setWidth(){
+    var boxwidth = document.querySelector(".container_o").offsetWidth;
+    var displaywidth = document.querySelector(".row_o").offsetWidth;
+    var displayheight = document.querySelector(".row_o").offsetHeight;
+    var children = document.querySelectorAll(".row-container > .container_o").length;
+    var outerboxwidth = children * boxwidth + (children*10);
+    document.querySelector(".row-container").style.width = outerboxwidth+"px";
+    document.querySelectorAll("button")[0].style.height = displayheight+"px";
+    document.querySelectorAll("button")[1].style.height = displayheight+"px";
+    maxMargin = outerboxwidth - displaywidth;
+}
+function slideLeft(event){
+    var rowcont = document.querySelector(".row-container");
+    if(right <= -maxMargin){
+        event.preventDefault();
+    }
+    else{
+        right -= jumpMargin;
+    }
+    rowcont.style.marginLeft = right+"px";
+}
+function slideRight(event){
+    var rowcont = document.querySelector(".row-container");
+    if(right==0){
+        event.preventDefault();
+    }
+    else if(right >= maxMargin){
+        event.preventDefault();
+    }
+    else{
+        right += jumpMargin;
+    }
+    rowcont.style.marginLeft = right+"px";
+}
+window.onload=setWidth;
